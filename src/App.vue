253fcab6c2c5734c7f41142f584f24c6693827b5
unknown <elizerlumilay@gmail.com>
2023-11-05 16:17:40 +0800

    Added vue basic structure
    Installed tailwind and setup required fonts

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import MainNav from "./components/MainNav.vue";
+</script>
+
+<template>
+  <MainNav />
+  <h1 class="text-5xl font-normal underline">Hello world!</h1>
+</template>
+
+<style scoped></style>
